0268aae974e66a36cfba458da5280728274615fe
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-12-06 16:52:42 +0100

    added Processor as scoped

---
+++ Api/Processor/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Processor
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddProcessor(this IServiceCollection service)
+        {
+            service.AddScoped<IProcessor, Processor>();
+        }
+    }
+}
