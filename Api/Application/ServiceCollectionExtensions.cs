5fc0d11b69024496e4d6a0ed0e3053c58016b94a
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-12-06 16:52:57 +0100

    added Mediatr and GenerateMemoryMapService as scoped

---
+++ Api/Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Application.Managments.PostProcessorProgram;
+using Application.Services;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddApplication(this IServiceCollection service)
+        {
+            service.AddMediatR(typeof(PostProcessorProgram));
+            service.AddScoped<IGenerateMemoryMapService, GenerateMemoryMapService>();
+        }
+    }
+}
