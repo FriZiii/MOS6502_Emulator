3cabb700644dab661a7bc873ae5404ad69b9e359
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-12-06 16:49:02 +0100

    Create ProcessorResultDto.cs

---
+++ Api/Application/Managments/ProcessorResultDto.cs
@@ -0,0 +1,14 @@
+﻿using Application.Services;
+
+namespace Application.Managments
+{
+    public class ProcessorResultDto
+    {
+        public int Accumulator { get; set; }
+        public int YRegister { get; set; }
+        public int XRegister { get; set; }
+        public int ProgramCounter { get; set; }
+        public string Program { get; set; }
+        public IEnumerable<MemoryRowModel> MemoryMap { get; set; }
+    }
+}
