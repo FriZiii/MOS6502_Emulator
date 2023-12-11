c46b8c25088aee54ad7ddce7762369cae7c54051
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-12-11 17:39:39 +0100

    implemented basic logic of emulator

--- Frontend/emulator6502/components/memory-map.js
+++ Frontend/emulator6502/components/memory-map.js
@@ -1,30 +1,37 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-
-const MemoryMap = ({ memoryMap}) => {
+const MemoryMap = ({ memoryMap }) => {
     return (
         <View style={styles.container}>
-            {memoryMap && memoryMap.map((item, index) => (
-                <View key={index} style={styles.row}>
-                    <Text style={styles.item}>{item.location00}</Text>
-                    <Text style={styles.item}>{item.location01}</Text>
-                    <Text style={styles.item}>{item.location02}</Text>
-                    <Text style={styles.item}>{item.location03}</Text>
-                    <Text style={styles.item}>{item.location04}</Text>
-                    <Text style={styles.item}>{item.location05}</Text>
-                    <Text style={styles.item}>{item.location06}</Text>
-                    <Text style={styles.item}>{item.location07}</Text>
-                    <Text style={styles.item}>{item.location08}</Text>
-                    <Text style={styles.item}>{item.location09}</Text>
-                    <Text style={styles.item}>{item.location0A}</Text>
-                    <Text style={styles.item}>{item.location0B}</Text>
-                    <Text style={styles.item}>{item.location0C}</Text>
-                    <Text style={styles.item}>{item.location0D}</Text>
-                    <Text style={styles.item}>{item.location0E}</Text>
-                    <Text style={styles.item}>{item.location0F}</Text>
+            {memoryMap && memoryMap.length > 0 ? (
+                memoryMap.map((item, index) => (
+                    <View key={index} style={styles.row}>
+                        <Text style={styles.item}>{item.location00}</Text>
+                        <Text style={styles.item}>{item.location01}</Text>
+                        <Text style={styles.item}>{item.location02}</Text>
+                        <Text style={styles.item}>{item.location03}</Text>
+                        <Text style={styles.item}>{item.location04}</Text>
+                        <Text style={styles.item}>{item.location05}</Text>
+                        <Text style={styles.item}>{item.location06}</Text>
+                        <Text style={styles.item}>{item.location07}</Text>
+                        <Text style={styles.item}>{item.location08}</Text>
+                        <Text style={styles.item}>{item.location09}</Text>
+                        <Text style={styles.item}>{item.location0A}</Text>
+                        <Text style={styles.item}>{item.location0B}</Text>
+                        <Text style={styles.item}>{item.location0C}</Text>
+                        <Text style={styles.item}>{item.location0D}</Text>
+                        <Text style={styles.item}>{item.location0E}</Text>
+                        <Text style={styles.item}>{item.location0F}</Text>
+                    </View>
+                ))
+            ) : (
+                <View style={styles.container}>
+                    <Text style={styles.message}>You have to load the program to see the memory map</Text>
+                    <Ionicons name="cog" size={164} color="black" />
                 </View>
-            ))}
+            )}
         </View>
     )
 }
@@ -36,10 +43,20 @@ const styles = StyleSheet.create({
         fontWeight: '500'
     },
     container: {
+        marginTop: 6,
+        paddingLeft: 20,
+        paddingRight: 20,
         flex: 1,
         flexDirection: 'column',
+        justifyContent: 'center',
         alignItems: "center",
         gap: 1
+    },
+    message: {
+        fontWeight: '500',
+        fontSize: 18,
+        alignItems: 'center',
+        textAlign: 'center'
     },
     row: {
         gap: 1,
--- Frontend/emulator6502/pages/emulator.js
+++ Frontend/emulator6502/pages/emulator.js
@@ -1,222 +1,297 @@
 import React, { useMemo, useState } from 'react';
-import { View, Text, StyleSheet, Button, TextInput, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput, Pressable, ActivityIndicator } from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import axios from "axios";
 
 import MemoryMap from '../components/memory-map';
 
+const Emulator = ({ naviagtion }) => {
+  const snapPoints = useMemo(() => ['8%', '55%'], []);
+  const [programInput, setProgramInput] = useState('');
 
-const Emulator = ({naviagtion}) => {
-    const snapPoints = useMemo(() => ['10%', '65%'], []);
+  const [result, setResult] = useState({
+    accumulator: 0,
+    yRegister: 0,
+    xRegister: 0,
+    programCounter: 0,
+    program: '169,20',
+    memoryMap: []
+  });
 
-    const [programInput, setProgramInput] = useState('');
-    return (
-        <GestureHandlerRootView  style={styles.container}>
-            <TextInput
-                placeholder="Enter code..."
-                editable
-                multiline
-                numberOfLines={8}
-                onChangeText={text => setProgramInput(text)}
-                value={programInput}
-                style={styles.input}
-            />
-            <View style={styles.buttonsContainer}>
-                <Pressable style={styles.buttonLeftStyle}><Text style={styles.buttonTextStyle}>Load Program</Text></Pressable>
-                <Pressable style={styles.buttonRightStyle}><Text style={styles.buttonTextStyle}>Next Step</Text></Pressable>
-            </View>
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-            <Text style={styles.propertiesheader}>Processor State</Text>
-            <View style={styles.propertiesContainerStyle}>
-              <View style={styles.propertyStyle}>
-                  <Text style={styles.popertyTextStyle}>Accumulator</Text><Text>$45</Text>
-              </View>
-              <View style={styles.propertyStyle}>
-                  <Text style={styles.popertyTextStyle}>Counter</Text><Text>5</Text>
-              </View>
-              <View style={styles.propertyStyle}>
-                  <Text style={styles.popertyTextStyle}>X Register</Text><Text>5</Text>
-              </View>
-              <View style={styles.propertyStyle}>
-                  <Text style={styles.popertyTextStyle}>Y Register</Text><Text>0</Text>
-              </View>
-            </View>
+  const nextStepHandler = async () => {
+    setIsLoading(true);
+    try {
+      await axios.post(`https://mos6502-api20231211161909.azurewebsites.net/nextStep`, {
+        "program": result.program,
+        "programCounter": result.programCounter
+      }).then((response) => {
+        setResult(response.data.result);
+        setIsLoading(false);
+      });
+    } catch (error) {
+      setError(error);
+      alert(`Error: ${error}`)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
-            <Text style={styles.propertiesheader}>Flags State</Text>
-            <View style={styles.flagsContainerStyle}>
-              <View style={styles.flagStyle}>
-                  <Text style={styles.flagTextStyle}>Negative Flag</Text><Text>1</Text>
-              </View>
-              <View style={styles.flagStyle}>
-                  <Text style={styles.flagTextStyle}>Overflow Flag</Text><Text>0</Text>
-              </View>
-              <View style={styles.flagStyle}>
-                  <Text style={styles.flagTextStyle}>Decimal Flag</Text><Text>1</Text>
-              </View>
-              <View style={styles.flagStyle}>
-                  <Text style={styles.flagTextStyle}>Zero Flag</Text><Text>0</Text>
-              </View>
-              <View style={styles.lonelyflagStyle}>
-                  <Text style={styles.flagTextStyle}>Carry Flag</Text><Text>0</Text>
-              </View>
-            </View>
-            
-        <BottomSheet style={styles.bottomSheet} snapPoints={snapPoints}>
-          <View style={styles.contentContainer}>
-            <Text>Memmory Map</Text>
-            <MemoryMap />
-          </View>
-        </BottomSheet>
-      </GestureHandlerRootView >
-    );
+  const loadProgramHandler = async () => {
+    console.log(result.memoryMap)
+    setIsLoading(true);
+    try {
+      await axios.post(`https://mos6502-api20231211161909.azurewebsites.net/loadProgram`, {
+        "program": programInput
+      }).then((response) => {
+        setResult(response.data.result);
+        setIsLoading(false);
+      });
+    } catch (error) {
+      setError(error);
+      alert(`Error: ${error}`)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <TextInput
+        placeholder="Enter code..."
+        editable
+        multiline
+        numberOfLines={8}
+        onChangeText={text => setProgramInput(text)}
+        value={programInput}
+        style={styles.input}
+      />
+      <View style={styles.buttonsContainer}>
+        <Pressable style={styles.buttonLeftStyle} onPress={loadProgramHandler}><Text style={styles.buttonTextStyle}>Load Program</Text></Pressable>
+        <Pressable style={styles.buttonRightStyle} onPress={nextStepHandler}><Text style={styles.buttonTextStyle}>Next Step</Text></Pressable>
+      </View>
+
+      <Text style={styles.propertiesheader}>Processor State</Text>
+      <View style={styles.propertiesContainerStyle}>
+        <View style={styles.propertyStyle}>
+          <Text style={styles.popertyTextStyle}>Accumulator</Text>
+          {isLoading ? (
+            <ActivityIndicator color="#000"/>
+          ) : (
+            <Text>{result.accumulator}</Text>
+          )}
+        </View>
+        <View style={styles.propertyStyle}>
+          <Text style={styles.popertyTextStyle}>Counter</Text>
+          {isLoading ? (
+            <ActivityIndicator color="#000"/>
+          ) : (
+            <Text>{result.programCounter}</Text>
+          )}
+        </View>
+        <View style={styles.propertyStyle}>
+          <Text style={styles.popertyTextStyle}>X Register</Text>
+          {isLoading ? (
+            <ActivityIndicator color="#000"/>
+          ) : (
+            <Text>{result.xRegister}</Text>
+          )}
+        </View>
+        <View style={styles.propertyStyle}>
+          <Text style={styles.popertyTextStyle}>Y Register</Text>
+          {isLoading ? (
+            <ActivityIndicator color="#000"/>
+          ) : (
+            <Text>{result.yRegister}</Text>
+          )}
+        </View>
+      </View>
+
+      <Text style={styles.propertiesheader}>Flags State</Text>
+      <View style={styles.flagsContainerStyle}>
+        <View style={styles.flagStyle}>
+          <Text style={styles.flagTextStyle}>Negative Flag</Text><Text>1</Text>
+        </View>
+        <View style={styles.flagStyle}>
+          <Text style={styles.flagTextStyle}>Overflow Flag</Text><Text>0</Text>
+        </View>
+        <View style={styles.flagStyle}>
+          <Text style={styles.flagTextStyle}>Decimal Flag</Text><Text>1</Text>
+        </View>
+        <View style={styles.flagStyle}>
+          <Text style={styles.flagTextStyle}>Zero Flag</Text><Text>0</Text>
+        </View>
+        <View style={styles.lonelyflagStyle}>
+          <Text style={styles.flagTextStyle}>Carry Flag</Text><Text>0</Text>
+        </View>
+      </View>
+
+      <BottomSheet style={styles.bottomSheet} snapPoints={snapPoints}>
+        <View style={styles.contentContainer}>
+          <Text style={styles.memmoryMapHeader}>Memmory Map</Text>
+          <MemoryMap memoryMap={result.memoryMap} />
+        </View>
+      </BottomSheet>
+    </GestureHandlerRootView >
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 24,
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+  input: {
+    textAlignVertical: 'top',
+    width: '100%',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#BCBCBC",
+    marginTop: 20,
+    marginBottom: 8,
+    borderRadius: 10,
+  },
+  buttonLeftStyle: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    backgroundColor: 'black',
+    marginRight: 10,
+  },
+  buttonRightStyle: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    backgroundColor: 'black',
+    marginLeft: 10,
+  },
+  buttonTextStyle: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  buttonsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  propertyStyle: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexBasis: '48%',
+    borderRadius: 15,
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    marginVertical: 5,
+    backgroundColor: '#fff',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    input: {
-        textAlignVertical: 'top',
-        width: '100%',
-        padding: 10,
-        borderWidth: 1,
-        borderColor: "#BCBCBC",
-        marginTop: 20,
-        marginBottom: 8,
-        borderRadius: 10,
-    },
-    buttonLeftStyle: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 10,
-      paddingHorizontal: 10,
-      borderRadius: 5,
-      backgroundColor: 'black',
-      marginRight: 10,
-    },
-    buttonRightStyle: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 10,
-      paddingHorizontal: 10,
-      borderRadius: 5,
-      backgroundColor: 'black',
-      marginLeft: 10,
-    },
-    buttonTextStyle: {
-      fontSize: 16,
-      lineHeight: 21,
-      fontWeight: 'bold',
-      letterSpacing: 0.25,
-      color: 'white',
-    },
-    buttonsContainer: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    propertyStyle: {
-      display: 'flex',
-      flexDirection: 'row',
-      flexBasis: '48%',
-      borderRadius: 15,
-      display: 'flex',
-      justifyContent: 'space-between',
-      paddingHorizontal: 15,
-      paddingVertical: 5,
-      marginVertical: 5,
-      backgroundColor: '#fff',
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.22,
-      shadowRadius: 2.22,
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
 
-      elevation: 5,
+    elevation: 5,
+  },
+  popertyTextStyle: {
+    fontWeight: 'bold',
+  },
+  propertiesContainerStyle: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  flagsContainerStyle: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  flagStyle: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexBasis: '48%',
+    borderRadius: 15,
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    marginVertical: 5,
+    backgroundColor: '#fff',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    popertyTextStyle: {
-      fontWeight: 'bold',
-    },
-    propertiesContainerStyle: {
-      display: 'flex',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'space-between',
-    },
-    flagsContainerStyle: {
-      display: 'flex',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'space-between',
-    },
-    flagStyle: {
-      display: 'flex',
-      flexDirection: 'row',
-      flexBasis: '48%',
-      borderRadius: 15,
-      display: 'flex',
-      justifyContent: 'space-between',
-      paddingHorizontal: 15,
-      paddingVertical: 5,
-      marginVertical: 5,
-      backgroundColor: '#fff',
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.22,
-      shadowRadius: 2.22,
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
 
-      elevation: 5,
+    elevation: 5,
+  },
+  lonelyflagStyle: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexBasis: '100%',
+    borderRadius: 15,
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+    paddingVertical: 5,
+    marginVertical: 5,
+    backgroundColor: '#fff',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    lonelyflagStyle: {
-      display: 'flex',
-      flexDirection: 'row',
-      flexBasis: '100%',
-      borderRadius: 15,
-      display: 'flex',
-      justifyContent: 'space-between',
-      paddingHorizontal: 15,
-      paddingVertical: 5,
-      marginVertical: 5,
-      backgroundColor: '#fff',
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.22,
-      shadowRadius: 2.22,
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
 
-      elevation: 5,
+    elevation: 5,
+  },
+  propertiesheader: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 30,
+    marginBottom: 5,
+  },
+  bottomSheet: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    propertiesheader: {
-      fontSize: 20,
-      textAlign: 'center',
-      marginTop: 30,
-      marginBottom: 5,
-    },
-    bottomSheet: {
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      
-      elevation: 5,
-    },
-    contentContainer: {
-      flex: 1,
-      alignItems: 'center',
-    },
-  });
-  
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  memmoryMapHeader: {
+    fontSize: 18,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+  }
+});
+
 export default Emulator;
